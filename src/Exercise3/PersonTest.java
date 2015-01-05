8e4860f3f2468106f4848f7c9e461619603ecd4f
Caleb <caleb@whataloadofrubbish.com>
2015-01-05 14:34:53 +0000

    added Exercise 3 Person class, Test class, and failing test case

---
+++ src/Exercise3/PersonTest.java
@@ -0,0 +1,24 @@
+package Exercise3;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+	@Test
+	public void testsNormalName() {
+		Person p = new Person();
+		String input = "Dereck Robert Yssirt";
+		String output = p.getInitials(input);
+		String expected = "DRY";
+		assertEquals(output, expected);
+	}
+	
+	@Test
+	public void testsNameWithDoubleSpaces() {
+		Person p = new Person();
+		String input = "Dereck  Robert  Yssirt";
+		String output = p.getInitials(input);
+		String expected = "DRY";
+		assertEquals(output, expected);
+	}
+}
