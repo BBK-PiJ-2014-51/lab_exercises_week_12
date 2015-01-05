1aee08bb8bc2252f4933c4db06f240848794443c
Caleb <caleb@whataloadofrubbish.com>
2015-01-05 14:43:06 +0000

    fixed bug, failing test case now passes

--- src/Exercise3/Person.java
+++ src/Exercise3/Person.java
@@ -4,7 +4,13 @@ public class Person{
 		String result = "";
 		String[] words = fullName.split(" ");
 		for (int i = 0; i < words.length; i++) {
-			String nextInitial = "" + words[i].charAt(0);
+			String nextInitial = "";
+			for (int j = 0; j < words[i].length(); j++){
+				if (words[i].charAt(j) != ' '){
+					nextInitial = "" + words[i].charAt(j);
+					break;
+				}
+			}
 			result = result + nextInitial.toUpperCase();
 		}	
 		return result;
