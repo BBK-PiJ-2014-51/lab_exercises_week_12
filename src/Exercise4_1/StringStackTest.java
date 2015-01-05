040a3187d5883df3ec9b771b2156f44a57c0c110
unknown <cclayt02@CSPC044.dcs.bbk.ac.uk>
2015-01-05 19:17:49 +0000

    started files + empty test cases

---
+++ src/Exercise4_1/StringStackTest.java
@@ -0,0 +1,59 @@
+package Exercise4_1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringStackTest {
+
+	@Test
+	public void testEmptyArrayStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEmptyPointerStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPushValuesArrayStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPushValuesPointerStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPeakValueArrayStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPeakValuePointerStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPopValueArrayStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPopValuePointerStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPopAllArrayStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testPopAllPointerStack() {
+		fail("Not yet implemented");
+	}
+
+}
